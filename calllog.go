fb79e23050db732e251e777407403fddb89875b7
imac <imac@imacdeiMac.local>
2023-04-04 22:01:17 +0800

    update middles

--- calllog.go
+++ calllog.go
@@ -4,25 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	rtime "github.com/r2day/base/time"
+	"github.com/r2day/body"
+	log "github.com/sirupsen/logrus"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	log "github.com/sirupsen/logrus"
 )
 
 const (
-	SYS_LOGIN_LOG = "sys_login_log"
-	defaultCallLogColl = "default_call_log"
+	SYS_LOGIN_LOG           = "sys_login_log"
+	defaultCallLogColl      = "default_call_log"
 	defaultOperationLogColl = "default_operation_log"
-	ignoreGET = "GET"
+	ignoreGET               = "GET"
 )
 
 var (
-	customCallLogColl = os.Getenv("CUSTOME_CALL_LOG_COLL")
+	customCallLogColl      = os.Getenv("CUSTOME_CALL_LOG_COLL")
 	customOperationLogColl = os.Getenv("CUSTOME_OPERATION_LOG_COLL")
 )
 
@@ -41,7 +44,7 @@ type CallLogData struct {
 	UpdatedUserId string `json:"updated_user_id" bson:"updated_user_id"`
 	// 状态
 	Status bool `json:"status"`
-	// 客户IP 
+	// 客户IP
 	ClientIP string `json:"client_ip" bson:"client_ip"`
 	// 远程IP
 	RemoteIP string `json:"remote_ip"  bson:"remote_ip"`
@@ -54,7 +57,6 @@ type CallLogData struct {
 	// 目标
 	TargetId string `json:"target_id"  bson:"target_id"`
 }
-
 
 // LoginRequest Binding from JSON
 type LoginRequest struct {
@@ -69,7 +71,7 @@ type LoginRequest struct {
 }
 
 // LoginLogMiddleware 登陆日志
-func LoginLogMiddleware(db * mongo.Database) gin.HandlerFunc {
+func LoginLogMiddleware(db *mongo.Database) gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
@@ -82,37 +84,123 @@ func LoginLogMiddleware(db * mongo.Database) gin.HandlerFunc {
 		respCode := c.Writer.Status()
 
 		logCtx := log.WithField("client_id", clientIP).
-		WithField("remote_ip", remoteIP).
-		WithField("full_path", fullPath).
-		WithField("resp_status", respCode)
+			WithField("remote_ip", remoteIP).
+			WithField("full_path", fullPath).
+			WithField("resp_status", respCode)
 
 		if c.Request.Method == ignoreGET {
 			logCtx.Debug("it is get method, we don't record it on database")
 			c.Next()
-			return 
+			return
 		}
 
 		// 声明表
 		coll := db.Collection(SYS_LOGIN_LOG)
 
-		var jsonInstance LoginRequest
-		if err := c.ShouldBindBodyWith(&jsonInstance, binding.JSON); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "login params no right"})
+		isSimpleSign, err := strconv.ParseBool(os.Getenv("IS_SIMPLE_SIGN"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if isSimpleSign {
+			var jsonInstance body.SimpleSignInRequest
+			if err := c.ShouldBindBodyWith(&jsonInstance, binding.JSON); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "login params no right"})
+				return
+			}
+
+			newOne := &CallLogData{}
+			// 基本查询条件
+			// newOne.MerchantId = jsonInstance.MerchantId
+			newOne.ID = primitive.NewObjectID()
+			newOne.UserId = jsonInstance.Phone
+
+			// 插入身份信息
+			createdAt := rtime.FomratTimeAsReader(time.Now().Unix())
+			whoChange := c.GetHeader("AccountId")
+			newOne.UserId = whoChange
+			newOne.UpdatedUserId = whoChange
+			newOne.CreatedAt = createdAt
+			newOne.UpdatedAt = createdAt
+			newOne.ClientIP = clientIP
+			newOne.RemoteIP = remoteIP
+			newOne.FullPath = fullPath
+			newOne.RespCode = respCode
+
+			// 写入数据库
+			// 插入记录
+			_, err := coll.InsertOne(c.Request.Context(), newOne)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"message": "failed to insert one", "error": err.Error()})
+				return
+			}
+		} else {
+			var jsonInstance body.SignInRequest
+			if err := c.ShouldBindBodyWith(&jsonInstance, binding.JSON); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "login params no right"})
+				return
+			}
+
+			newOne := &CallLogData{}
+			// 基本查询条件
+			newOne.MerchantId = jsonInstance.MerchantId
+			newOne.ID = primitive.NewObjectID()
+			newOne.UserId = jsonInstance.Phone
+
+			// 插入身份信息
+			createdAt := rtime.FomratTimeAsReader(time.Now().Unix())
+			whoChange := c.GetHeader("AccountId")
+			newOne.UserId = whoChange
+			newOne.UpdatedUserId = whoChange
+			newOne.CreatedAt = createdAt
+			newOne.UpdatedAt = createdAt
+			newOne.ClientIP = clientIP
+			newOne.RemoteIP = remoteIP
+			newOne.FullPath = fullPath
+			newOne.RespCode = respCode
+
+			// 写入数据库
+			// 插入记录
+			_, err := coll.InsertOne(c.Request.Context(), newOne)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"message": "failed to insert one", "error": err.Error()})
+				return
+			}
+
+		}
+	}
+}
+
+// CallLogMiddleware 调用日志
+func CallLogMiddleware(db *mongo.Database) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		if c.Request.Method == ignoreGET {
+			fmt.Println("it is get method ,no data change so don't need to record it by default")
+			c.Next()
 			return
 		}
 
+		if customOperationLogColl == "" {
+			customOperationLogColl = defaultOperationLogColl
+		}
 
+		// 声明表
+		coll := db.Collection(customOperationLogColl)
 
+		clientIP := c.ClientIP()
+		remoteIP := c.RemoteIP()
+		fullPath := c.FullPath()
 
 		newOne := &CallLogData{}
 		// 基本查询条件
-		newOne.MerchantId = jsonInstance.MerchantId
+		newOne.MerchantId = c.GetHeader("MerchantId")
 		newOne.ID = primitive.NewObjectID()
-		newOne.UserId = jsonInstance.Phone
 
 		// 插入身份信息
 		createdAt := rtime.FomratTimeAsReader(time.Now().Unix())
-		whoChange :=  c.GetHeader("AccountId")
+		whoChange := c.GetHeader("AccountId")
 		newOne.UserId = whoChange
 		newOne.UpdatedUserId = whoChange
 		newOne.CreatedAt = createdAt
@@ -120,7 +208,8 @@ func LoginLogMiddleware(db * mongo.Database) gin.HandlerFunc {
 		newOne.ClientIP = clientIP
 		newOne.RemoteIP = remoteIP
 		newOne.FullPath = fullPath
-		newOne.RespCode = respCode
+		newOne.Method = method
+		newOne.TargetId = c.Param("_id")
 
 		// 写入数据库
 		// 插入记录
@@ -129,58 +218,6 @@ func LoginLogMiddleware(db * mongo.Database) gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"message": "failed to insert one", "error": err.Error()})
 			return
 		}
-	}
-}
-
-// CallLogMiddleware 调用日志
-func CallLogMiddleware(db * mongo.Database) gin.HandlerFunc {
-
-
-	return func(c *gin.Context) {
-	method := c.Request.Method
-	if c.Request.Method == ignoreGET {
-		fmt.Println("it is get method ,no data change so don't need to record it by default")
 		c.Next()
-		return 
-	}
-
-	if (customOperationLogColl == "") {
-		customOperationLogColl = defaultOperationLogColl
-	}
-
-	// 声明表
-	coll := db.Collection(customOperationLogColl)
-
-	clientIP := c.ClientIP()
-	remoteIP := c.RemoteIP()
-	fullPath := c.FullPath()
-
-
-	newOne := &CallLogData{}
-	// 基本查询条件
-	newOne.MerchantId = c.GetHeader("MerchantId")
-	newOne.ID = primitive.NewObjectID()
-
-	// 插入身份信息
-	createdAt := rtime.FomratTimeAsReader(time.Now().Unix())
-	whoChange :=  c.GetHeader("AccountId")
-	newOne.UserId = whoChange
-	newOne.UpdatedUserId = whoChange
-	newOne.CreatedAt = createdAt
-	newOne.UpdatedAt = createdAt
-	newOne.ClientIP = clientIP
-	newOne.RemoteIP = remoteIP
-	newOne.FullPath = fullPath
-	newOne.Method = method
-	newOne.TargetId = c.Param("_id")
-
-	// 写入数据库
-	// 插入记录
-	_, err := coll.InsertOne(c.Request.Context(), newOne)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "failed to insert one", "error": err.Error()})
-		return
-	}
-	c.Next()
 	}
 }
